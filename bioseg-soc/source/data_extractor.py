ae505552f82603085f127988e57cf4c2a0c05c57
fonsecamatheus <matheusrdf1997@gmail.com>
2024-10-14 13:41:39 -0400

    refatorando classe DataExtractor

--- bioseg-soc/source/data_extractor.py
+++ bioseg-soc/source/data_extractor.py
@@ -1,49 +1,57 @@
 from datetime import date
 
-from source.config import Config
 from source.http_requester import HttpRequester
+from utils.config import Config
 
 
 class DataExtractor:
     def __init__(self, requester: HttpRequester):
         self.requester = requester
-        self.anos = [ano for ano in range(2020, date.today().year+1)]
+        self.years = [year for year in range(2020, date.today().year+1)]
 
-    def extrair_empresas(self):
+    def extract_empresas(self):
         param = f'{{"empresa":"887992","codigo":"151534","chave":"{Config.KEY_SOC_EMPRESAS}","tipoSaida":"json","codigoEmpresa":"","codigoUnidade":"","dataInicio":"","dataFim":"","codigoGrupoProduto":"","estadoUnidade":"","codigoSubGrupo":"","diasAVencer":"60","codigoProduto":"2","statusEmpresa":"","statusUnidade":""}}'
         url = f"{Config.BASE_URL}{param}"
         return self.requester.get_data(url)
     
-    def extrair_funcionarios(self, cod_empresa):
+    def extract_funcionarios(self, cod_empresa):
         param = f'{{"empresa":"{cod_empresa}","codigo":"138566","chave":"{Config.KEY_SOC_FUNCIONARIOS}","tipoSaida":"json","ativo":"Sim","inativo":"Sim","afastado":"Sim","pendente":"Sim","ferias":"Sim"}}'
         url = f"{Config.BASE_URL}{param}"
         return self.requester.get_data(url)
 
-    def extrair_exames_convocacao(self, cod_empresa, codigos_funcionarios):
-        param = f'{{"empresa":"887992","codigo":"149461","chave":"{Config.KEY_SOC_EXAMES_CONVOCACAO}","tipoSaida":"json","empresaTrabalho":"{cod_empresa}","funcionarios":"{codigos_funcionarios}","periodo":"12/2024","exame":"","convocarClinico":"0","nuncaRealizados":"1","periodicosNuncaRealizados":"1","selecao":"1","examesPendentes":"0","convocaPendentesPCMSO":"0"}}'
+    def extract_exames_convocacao(self, cod_empresa, cod_funcionarios):
+        param = f'{{"empresa":"887992","codigo":"149461","chave":"{Config.KEY_SOC_EXAMES_CONVOCACAO}","tipoSaida":"json","empresaTrabalho":"{cod_empresa}","funcionarios":"{cod_funcionarios}","periodo":"12/2024","exame":"","convocarClinico":"0","nuncaRealizados":"1","periodicosNuncaRealizados":"1","selecao":"1","examesPendentes":"0","convocaPendentesPCMSO":"0"}}'
         url = f"{Config.BASE_URL}{param}"
         return self.requester.get_data(url)
 
-    def extrair_exames_realizados(self, cod_empresa):
-        for ano in self.anos:
-            param = f'{{"empresa":"887992","codigo":"138569","chave":"{Config.KEY_SOC_EXAMES_REALIZADOS}","tipoSaida":"json","empresaTrabalho":"{cod_empresa}","dataInicio":"01/01/{ano}","dataFim":"31/12/{ano}"}}'
+    def extract_exames_realizados(self, cod_empresa):
+        all_exames_realizados = []  
+        for year in self.years:
+            param = f'{{"empresa":"887992","codigo":"138569","chave":"{Config.KEY_SOC_EXAMES_REALIZADOS}","tipoSaida":"json","empresaTrabalho":"{cod_empresa}","dataInicio":"01/01/{year}","dataFim":"31/12/{year}"}}'
             url = f"{Config.BASE_URL}{param}"
-            return self.requester.get_data(url)
+            exames_realizados = self.requester.get_data(url)
+            all_exames_realizados.extend(exames_realizados)  
+        return all_exames_realizados  
 
-    def extrair_visao_bioseg(self, cod_empresa):
-        param = f'{{"empresa":"{cod_empresa}","codigo":"139629","chave":"{Config.KEY_SOC_ADICIONAIS_VISAO_BIOSEG}","tipoSaida":"json","funcionario":"1-999999999","situacaoFuncionario":"1"}}'
+    def extract_funcionarios_expostos(self, cod_empresa):
+        param = f'{{"empresa":"{cod_empresa}","codigo":"139629","chave":"{Config.KEY_SOC_FUNCIONARIOS_EXPOSTOS}","tipoSaida":"json","funcionario":"1-999999999","situacaoFuncionario":"1"}}'
         url = f"{Config.BASE_URL}{param}"
         return self.requester.get_data(url)
 
-    def extrair_esocial(self, cod_empresa):
-        for ano in self.anos:
-            param = f'{{"empresa":"887992","codigo":"143109","chave":"{Config.KEY_SOC_ESOCIAL}","tipoSaida":"json","empresaTrabalho":"{cod_empresa}","dataInicio":"01/01/{ano}","dataFim":"31/12/{ano}","status":"99","layout":"0","unidade":"0","ambiente":"1"}}'
+    def extract_eventos_esocial(self, cod_empresa):
+        all_eventos_esocial = []  
+        for year in self.years:
+            param = f'{{"empresa":"887992","codigo":"143109","chave":"{Config.KEY_SOC_EVENTOS_ESOCIAL}","tipoSaida":"json","empresaTrabalho":"{cod_empresa}","dataInicio":"01/01/{year}","dataFim":"31/12/{year}","status":"99","layout":"0","unidade":"0","ambiente":"1"}}'
             url = f"{Config.BASE_URL}{param}"
-            return self.requester.get_data(url)
+            eventos_esocial = self.requester.get_data(url)
+            all_eventos_esocial.extend(eventos_esocial) 
+        return all_eventos_esocial  
 
-    def extrair_cat(self):
-        for ano in self.anos:
-            param = f'{{"empresa":"887992","codigo":"163423","chave":"{Config.KEY_SOC_CAT}","tipoSaida":"json","dataInicio":"01/01/{ano}","dataFim":"31/12/{ano}"}}'
+    def extract_cats(self):
+        all_cats = [] 
+        for year in self.years:
+            param = f'{{"empresa":"887992","codigo":"163423","chave":"{Config.KEY_SOC_CAT}","tipoSaida":"json","dataInicio":"01/01/{year}","dataFim":"31/12/{year}"}}'
             url = f'{Config.BASE_URL}{param}'
-            return self.requester.get_data(url)
-
+            cats = self.requester.get_data(url)
+            all_cats.extend(cats)  
+        return all_cats 
