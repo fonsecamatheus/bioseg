e716feeed3cff0ce6365c99369ba9a6c09db57e9
fonsecamatheus <matheusrdf1997@gmail.com>
2024-10-25 11:31:50 -0400

    implementando o pymysql no método

--- bioseg-ugo/source/connect_mysql.py
+++ bioseg-ugo/source/connect_mysql.py
@@ -1,20 +1,22 @@
 import os
 
-import mysql.connector
+import pymysql
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def connect_mysql():
     try:
-        connection = mysql.connector.connect(
+        connection = pymysql.connect(
             host=os.getenv('host'),
             database=os.getenv('database'),
             user=os.getenv('user'),
             password=os.getenv('password')
         )
         return connection
-    except mysql.connector.Error as e:
-        print(f'Erro ao conectar ao banco mysql: {e}')
+    except pymysql.MySQLError as e:
+        print(f'Erro ao conectar ao banco MySQL: {e}')
+        return None
+    except ValueError as ve:
+        print(f'Erro de configuração: {ve}')
         return None
